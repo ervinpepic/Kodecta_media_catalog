d78d21c8c13a5281383bfaee21b78937afce3ad8
Ervin Pepic <ervin.hack@gmail.com>
2019-12-13 21:00:53 +0100

    Installed Django, preparing for database connetion

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ervinpepic/Documents/Development/WebApps/DjangoApps/Kodecta_media_catalog/bin/python3.8
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
