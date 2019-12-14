10d5db5e0a7d4ff0e5218efb7eb225b628f38910
Ervin Pepic <ervin.hack@gmail.com>
2019-12-14 13:48:44 +0100

    Database setup done! Installed Django-Rest-Framework and other s stuff that project need.

--- MediaCatalog/MediaCatalog/settings.py
+++ MediaCatalog/MediaCatalog/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'MediaCatalog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "kodecta_media_catalog",
+        "USER": "ervin",
+        "PASSWORD": "2110",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
