b80ec4d46d75cca4b600a59c661c813abafed2b2
Ervin Pepic <ervin.hack@gmail.com>
2019-12-15 19:47:59 +0100

    Created models representation abstarction in models.py for database

--- MediaCatalog/media_catalog/models.py
+++ MediaCatalog/media_catalog/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Creator(models.Model):
+	name 				= models.CharField(max_length=50)
+	date_of_birth		= models.DateField(auto_now_add=False, auto_now=False)
+
+	class Meta:
+		verbose_name = "Category"
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.name
+
+
+
+class Provider(models.Model):
+	name			= models.CharField(max_length=30)
+	provider_rank 	= models.IntegerField(default=0)
+
+
+	class Meta:
+		verbose_name = "Provider"
+		verbose_name_plural = "Providers"
+
+	def __str__(self):
+		return self.provider_name
+
+
+class Category(models.Model):
+	name		= models.CharField(max_length=25)
+	description	= models.TextField()
+
+
+	class Meta:
+		verbose_name = "Category"
+		verbose_name_plural = "Categories"
+
+	def __str__(self):
+		return self.category_name
+
+class Media(models.Model):
+	title 					= models.CharField(max_length=120)
+	artist 					= models.ManyToManyField(Creator)
+	publisher 				= models.ManyToManyField(Provider)
+	originating_country 	= models.CharField(max_length=50)
+	genre 					= models.ManyToManyField(Category,)
+	publishing_date			= models.DateField(auto_now=False, auto_now_add=False)
+	date_published			= models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name = "Media"
+		verbose_name_plural = "Medias"
+
+	def __str__(self):
+		return self.title
+
+
+    
+    
+
+    
