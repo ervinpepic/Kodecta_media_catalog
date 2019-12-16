cb2816856ed6a9b4fea3e8f7db535b5eab6f6c58
Ervin Pepic <ervin.hack@gmail.com>
2019-12-16 20:09:36 +0100

    url changed

--- MediaCatalog/media_catalog/urls.py
+++ MediaCatalog/media_catalog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'media_catalog'
 
 urlpatterns = [
-    path('media-api/', views.MediaList.as_view(), name='index'),
-    path('media-api/<int:pk>/', views.MediaDetail.as_view(), name='media-detail'),
+    path('api/media/', views.MediaList.as_view(), name='index'),
+    path('api/media/<int:pk>/', views.MediaDetail.as_view(), name='media-detail'),
 ]
