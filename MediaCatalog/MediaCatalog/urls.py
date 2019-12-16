04d2d94e704864a08b1e75e4172fd16a9b1cf669
Ervin Pepic <ervin.hack@gmail.com>
2019-12-16 22:50:33 +0100

    Repaired urls

--- MediaCatalog/MediaCatalog/urls.py
+++ MediaCatalog/MediaCatalog/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from media_catalog import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # url(r'^api-auth/', include('rest_framework.urls')),
     path('', include('media_catalog.urls')),
     
 ]
